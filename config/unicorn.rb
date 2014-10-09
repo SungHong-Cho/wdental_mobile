349e81889bf5fa8740d53aeda78e52cc848639ee
SungHong Cho <lunaticbreeze@gmail.com>
2014-10-09 18:06:39 +0900

    edit unicorn option root path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/boa_incognito/current"
+root = "/home/deployer/apps/wdental_mobile/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.boa_incognito.sock"
+listen "/tmp/unicorn.wdental_mobile.sock"
 worker_processes 4
 timeout 30
