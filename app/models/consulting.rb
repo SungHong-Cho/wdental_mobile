d8f18b9cb7989317eded17ac8ef344e867f692f6
darkzin <tony@skyseed.me>
2015-01-07 02:20:26 +0900

    add consulting.rb file.

---
+++ app/models/consulting.rb
@@ -0,0 +1,12 @@
+class Consulting
+include Mongoid::Document
+include Mongoid::Timestamps
+include Mongoid::Paranoia
+field :name, type: String
+field :number, type: String
+field :email, type: String
+field :datetime, type: DateTime
+field :file_name, type: String, default: ""
+field :comment, type: String
+mount_uploader :image, ImageUploader
+end
